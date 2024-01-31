5cc88542eec6feb156c2d7d99e2ee7d7bc8a65c8
IagoraNz <iago.almeida@ufpi.edu.br>
2024-01-31 15:22:25 -0300

    Backup

--- main.pl
+++ main.pl
@@ -32,12 +32,14 @@ diabetes([reinaldo, masculino, 27.0, nao, nao, passado, 27.32, 3.5, 100], nao).
 diabetes([frederico, masculino, 54.0, nao, nao, passado, 30.41, 5.0, 158], nao).
 
 main :-
-    nl,
+    nl, nl,
     write('HOSPITAL SIRIO-PIAUIES'), nl,
     write('1 - Adicionar paciente'), nl,
     write('2 - Listar pacientes'), nl,
     write('3 - Calcular IMC'), nl,
-    write('4 - Sair'), nl,
+    write('4 - Editar paciente'), nl,
+    write('5 - Remover paciente'), nl,
+    write('6 - Sair'), nl,
     write('Escolha uma opcao: '),
     read(Opcao),    
     escolher_opcao(Opcao).
@@ -74,10 +76,29 @@ escolher_opcao(2) :-
     listar_pacientes,
     main.
 
+escolher_opcao(3) :-
+    calcular_imc(Imc),
+    write('IMC: '), write(Imc), nl,
+    main.
+
+escolher_opcao(4) :-
+    editar_paciente,
+    main.
+
+escolher_opcao(5) :-
+    write('Digite o nome do paciente que deseja remover: '),
+    read(Nome),
+    retract(diabetes([Nome, _, _, _, _, _, _, _, _], _)),
+    write('Paciente removido com sucesso!'), nl,
+    main.
+
+escolher_opcao(6) :-
+    write('Ate logo!'), nl.
+
 adicionar_paciente([Nome, Sexo, Idade, Hipertensao, Cardiaco, Fumante, IMC, Hemoglobina, Glicose], Diabetes) :-
     \+ diabetes(Nome, _),
     assertz(diabetes([Nome, Sexo, Idade, Hipertensao, Cardiaco, Fumante, IMC, Hemoglobina, Glicose], Diabetes)),
-    write('Paciente adicionado com sucesso!').
+    write('Paciente adicionado com sucesso!'), nl.
 
 adicionar_paciente([Nome, _, _, _, _, _, _, _, _], _) :-
     diabetes(Nome, _),
@@ -98,3 +119,92 @@ listar_pacientes :-
     nl,
     fail.
 listar_pacientes.
+
+editar_paciente :-
+    write('Digite o nome do paciente que deseja editar: '),
+    read(Nome),
+    write('Digite qual campo deseja editar: '),
+    read(Campo),
+    (
+        Campo = 'nome' -> write('Digite o novo nome: '), read(NovoNome), editar_nome(Nome, NovoNome);
+        Campo = 'sexo' -> write('Digite o novo sexo: '), read(NovoSexo), editar_sexo(Nome, NovoSexo);
+        Campo = 'idade' -> write('Digite a nova idade: '), read(NovaIdade), editar_idade(Nome, NovaIdade);
+        Campo = 'hipertensao' -> write('Digite a nova situacao de hipertensao: '), read(NovaHipertensao), editar_hipertensao(Nome, NovaHipertensao);
+        Campo = 'cardiaco' -> write('Digite a nova situacao cardiaca: '), read(NovaCardiaco), editar_cardiaco(Nome, NovaCardiaco);
+        Campo = 'fumante' -> write('Digite a nova situacao de fumante: '), read(NovoFumante), editar_fumante(Nome, NovoFumante);
+        Campo = 'imc' -> write('Digite o novo IMC: '), read(NovoImc), editar_imc(Nome, NovoImc);
+        Campo = 'hemoglobina' -> write('Digite a nova hemoglobina: '), read(NovaHemoglobina), editar_hemoglobina(Nome, NovaHemoglobina);
+        Campo = 'glicose' -> write('Digite a nova glicose: '), read(NovaGlicose), editar_glicose(Nome, NovaGlicose);
+        Campo = 'diabetes' -> write('Digite a nova situacao de diabetes: '), read(NovoDiabetes), editar_diabetes(Nome, NovoDiabetes)
+    ).
+
+editar_nome(Nome, NovoNome) :-
+    retract(diabetes([Nome, Sexo, Idade, Hipertensao, Cardiaco, Fumante, IMC, Hemoglobina, Glicose], Diabetes)),
+    assertz(diabetes([NovoNome, Sexo, Idade, Hipertensao, Cardiaco, Fumante, IMC, Hemoglobina, Glicose], Diabetes)),
+    write('Nome editado com sucesso!'), nl.
+editar_nome(_, _) :-
+    write('Paciente nao encontrado!'), nl.
+
+editar_sexo(Nome, NovoSexo) :-
+    retract(diabetes([Nome, _, Idade, Hipertensao, Cardiaco, Fumante, IMC, Hemoglobina, Glicose], Diabetes)),
+    assertz(diabetes([Nome, NovoSexo, Idade, Hipertensao, Cardiaco, Fumante, IMC, Hemoglobina, Glicose], Diabetes)),
+    write('Sexo editado com sucesso!'), nl.
+editar_sexo(_, _) :-
+    write('Paciente nao encontrado!'), nl.
+
+editar_idade(Nome, NovaIdade) :-
+    retract(diabetes([Nome, Sexo, _, Hipertensao, Cardiaco, Fumante, IMC, Hemoglobina, Glicose], Diabetes)),
+    assertz(diabetes([Nome, Sexo, NovaIdade, Hipertensao, Cardiaco, Fumante, IMC, Hemoglobina, Glicose], Diabetes)),
+    write('Idade editada com sucesso!'), nl.
+editar_idade(_, _) :-
+    write('Paciente nao encontrado!'), nl.
+
+editar_hipertensao(Nome, NovaHipertensao) :-
+    retract(diabetes([Nome, Sexo, Idade, _, Cardiaco, Fumante, IMC, Hemoglobina, Glicose], Diabetes)),
+    assertz(diabetes([Nome, Sexo, Idade, NovaHipertensao, Cardiaco, Fumante, IMC, Hemoglobina, Glicose], Diabetes)),
+    write('Hipertensao editada com sucesso!'), nl.
+editar_hipertensao(_, _) :-
+    write('Paciente nao encontrado!'), nl.
+
+editar_cardiaco(Nome, NovaCardiaco) :-
+    retract(diabetes([Nome, Sexo, Idade, Hipertensao, _, Fumante, IMC, Hemoglobina, Glicose], Diabetes)),
+    assertz(diabetes([Nome, Sexo, Idade, Hipertensao, NovaCardiaco, Fumante, IMC, Hemoglobina, Glicose], Diabetes)),
+    write('Problema cardiaco editado com sucesso!'), nl.
+editar_cardiaco(_, _) :-
+    write('Paciente nao encontrado!'), nl.
+
+editar_fumante(Nome, NovoFumante) :-
+    retract(diabetes([Nome, Sexo, Idade, Hipertensao, Cardiaco, _, IMC, Hemoglobina, Glicose], Diabetes)),
+    assertz(diabetes([Nome, Sexo, Idade, Hipertensao, Cardiaco, NovoFumante, IMC, Hemoglobina, Glicose], Diabetes)),
+    write('Fumante editado com sucesso!'), nl.
+editar_fumante(_, _) :-
+    write('Paciente nao encontrado!'), nl.
+
+editar_imc(Nome, NovoImc) :-
+    write('Deseja calcular o IMC? (s/n): '),
+    read(Calcular),
+    (
+        Calcular = 's' -> calcular_imc(NovoImc);
+        Calcular = 'n' -> true
+    ),
+    retract(diabetes([Nome, Sexo, Idade, Hipertensao, Cardiaco, Fumante, _, Hemoglobina, Glicose], Diabetes)),
+    assertz(diabetes([Nome, Sexo, Idade, Hipertensao, Cardiaco, Fumante, NovoImc, Hemoglobina, Glicose], Diabetes)),
+    write('IMC editado com sucesso!'), nl.
+
+editar_hemoglobina(Nome, NovaHemoglobina) :-
+    retract(diabetes([Nome, Sexo, Idade, Hipertensao, Cardiaco, Fumante, IMC, _, Glicose], Diabetes)),
+    assertz(diabetes([Nome, Sexo, Idade, Hipertensao, Cardiaco, Fumante, IMC, NovaHemoglobina, Glicose], Diabetes)),
+    write('Hemoglobina editada com sucesso!'), nl.
+
+editar_glicose(Nome, NovaGlicose) :-
+    retract(diabetes([Nome, Sexo, Idade, Hipertensao, Cardiaco, Fumante, IMC, Hemoglobina, _], Diabetes)),
+    assertz(diabetes([Nome, Sexo, Idade, Hipertensao, Cardiaco, Fumante, IMC, Hemoglobina, NovaGlicose], Diabetes)),
+    write('Glicose editada com sucesso!'), nl.
+
+editar_diabetes(Nome, NovoDiabetes) :-
+    retract(diabetes([Nome, Sexo, Idade, Hipertensao, Cardiaco, Fumante, IMC, Hemoglobina, Glicose], _)),
+    assertz(diabetes([Nome, Sexo, Idade, Hipertensao, Cardiaco, Fumante, IMC, Hemoglobina, Glicose], NovoDiabetes)),
+    write('Diabetes editada com sucesso!'), nl.
+editar_diabetes(_, _) :-
+    write('Paciente nao encontrado!'), nl.
+
